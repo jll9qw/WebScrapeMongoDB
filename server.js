411f38cacb8f6998c07aeeb1a165978197e292c1
jll9qw <lloyd804@gmail.com>
2019-06-17 20:09:18 -0400

    added mongodb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 const app = express();
 // First, tell the console what server3.js is doing
 console.log("\n******************************************\n" +
