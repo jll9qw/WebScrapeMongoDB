e403b181cd49a0e721f0f4773fb5de20dfcafd99
jll9qw <lloyd804@gmail.com>
2019-06-17 20:00:12 -0400

    installed node modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// Dependencies
+const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
-
+const app = express();
 // First, tell the console what server3.js is doing
 console.log("\n******************************************\n" +
             "Look at the image of every award winner in \n" +
