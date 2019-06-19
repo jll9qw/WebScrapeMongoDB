a2df0105dc3ce3cf036524ff1ce2f92ecbb5836c
jll9qw <lloyd804@gmail.com>
2019-06-19 19:23:41 -0400

    added mongoose connect

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const exphbs = require("express-handlebars");
 const app = express();
+
+mongoose.connect(MONGODB_URI);
 // First, tell the console what server3.js is doing
 console.log("\n******************************************\n" +
             "Look at the image of every award winner in \n" +
@@ -11,3 +14,14 @@ console.log("\n******************************************\n" +
             "grab the image's source URL." +
             "\n******************************************\n");
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
