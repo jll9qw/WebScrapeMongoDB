f8322815078285cfddc689b886815b603109afbe
jll9qw <lloyd804@gmail.com>
2019-07-06 05:28:45 -0400

    added mongoose to server

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 // Dependencies
 const express = require("express");
+let PORT = process.env.PORT || 3000;
 const cheerio = require("cheerio");
+const mongoose = require('mongoose');
 const axios = require("axios");
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 const exphbs = require("express-handlebars");
 const app = express();
+const router = express.Router();
 
-mongoose.connect(MONGODB_URI);
+
+
 // First, tell the console what server3.js is doing
 console.log("\n******************************************\n" +
-            "Look at the image of every award winner in \n" +
-            "one of the pages of `awwwards.com`. Then,\n" +
-            "grab the image's source URL." +
-            "\n******************************************\n");
+            "Connected! \n" );
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(router);
 
 
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Connection to Mongoose
+mongoose.connect(db, function(err){
+    if(err){
+        console.log("ERROR")
+    }else{
+        console.log("Connected with Mongoose")
+    }
+});
+
+app.listen(PORT, function(){
+    console.log("Listening on PORT:  "+ PORT);
+})
