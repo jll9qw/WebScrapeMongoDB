8b83fe9fecfc3eef8e46d27cfa21f45beb734c13
jll9qw <lloyd804@gmail.com>
2019-07-09 04:57:34 -0400

    added routes

--- config/routes.js
+++ config/routes.js
@@ -1,22 +1,10 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
-});
+module.exports = function(router){
+  router.get("/", function(req, res){
+    res.render("home"); 
+  });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
+  router.get("/saved", function(router){
+    res.render("saved");
+  })
+}
